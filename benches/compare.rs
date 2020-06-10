43e785b5564d51c41b823a801d70e7a2253bf549
Benjamin Bouvier <public@benj.me>
2020-06-10 16:44:30 +0200

    Add new wasm test cases, update format, allow running a dir as a bench;

--- benches/compare.rs
+++ benches/compare.rs
@@ -12,7 +12,13 @@ fn read(path: &str) -> IRSnapshot {
     bincode::deserialize(&contents).expect("couldn't deserialize")
 }
 
-fn run_group(c: &mut Criterion, sample_size: Option<usize>, lsra_opts: &Options, bt_opts: &Options, snapshot_path: &str,) {
+fn run_file(
+    c: &mut Criterion,
+    sample_size: Option<usize>,
+    lsra_opts: &Options,
+    bt_opts: &Options,
+    snapshot_path: &str,
+) {
     let mut group = c.benchmark_group(snapshot_path);
     if let Some(sample_size) = sample_size {
         group.sample_size(sample_size);
@@ -37,6 +43,51 @@ fn run_group(c: &mut Criterion, sample_size: Option<usize>, lsra_opts: &Options,
     });
 }
 
+fn run_dir(
+    c: &mut Criterion,
+    sample_size: Option<usize>,
+    lsra_opts: &Options,
+    bt_opts: &Options,
+    snapshot_path: &str,
+) {
+    let mut group = c.benchmark_group(snapshot_path);
+    if let Some(sample_size) = sample_size {
+        group.sample_size(sample_size);
+    }
+
+    let path = format!("./snapshots/{}", snapshot_path);
+    let mut snapshots = Vec::new();
+    for entry in fs::read_dir(path).unwrap() {
+        let path = entry.unwrap().path();
+        assert!(!path.is_dir());
+        snapshots.push(read(path.into_os_string().to_str().unwrap()));
+    }
+
+    group.bench_function("lsra", |b| {
+        b.iter_batched(
+            || (snapshots.clone(), lsra_opts.clone()),
+            |(snapshots, opts)| {
+                for mut snapshot in snapshots {
+                    snapshot.allocate(opts.clone()).unwrap();
+                }
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+
+    group.bench_function("bt", |b| {
+        b.iter_batched(
+            || (snapshots.clone(), bt_opts.clone()),
+            |(snapshots, opts)| {
+                for mut snapshot in snapshots {
+                    snapshot.allocate(opts.clone()).unwrap();
+                }
+            },
+            criterion::BatchSize::SmallInput,
+        )
+    });
+}
+
 pub fn run_bench(c: &mut Criterion) {
     let lsra_opts = Options {
         run_checker: false,
@@ -48,9 +99,11 @@ pub fn run_bench(c: &mut Criterion) {
         algorithm: Algorithm::Backtracking(BacktrackingOptions::default()),
     };
 
-    run_group(c, None, &lsra_opts, &bt_opts, "arm-clif/small");
-    run_group(c, None, &lsra_opts, &bt_opts, "arm-clif/medium");
-    run_group(c, Some(10), &lsra_opts, &bt_opts, "arm-clif/big");
+    run_file(c, None, &lsra_opts, &bt_opts, "arm-clif/small");
+    run_file(c, None, &lsra_opts, &bt_opts, "arm-clif/medium");
+    run_file(c, Some(10), &lsra_opts, &bt_opts, "arm-clif/big");
+    run_dir(c, Some(20), &lsra_opts, &bt_opts, "bz2-wasm");
+    run_dir(c, Some(10), &lsra_opts, &bt_opts, "regex-rs-wasm");
 }
 
 criterion_group!(benches, run_bench);
